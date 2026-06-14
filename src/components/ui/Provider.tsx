652189a139a4a5f85a3bebdec7af5753f2040a81
Ubonisrael Akpanudoh <akpanudohubonisrael@gmail.com>
2026-06-14 18:01:59 +0100

    correct auth provider path in Provider.tsx

--- src/components/ui/Provider.tsx
+++ src/components/ui/Provider.tsx
@@ -5,7 +5,7 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "sonner";
 import { ColorModeProvider, type ColorModeProviderProps } from "./ColorMode";
 import { BrowserRouter } from "react-router-dom";
-import { AuthProvider } from "@/hooks/useAuth";
+import { AuthProvider } from "@/providers/AuthProvider";
 
 const queryClient = new QueryClient();
 
